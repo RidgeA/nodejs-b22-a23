e133146547e759ada6e54c43e9e1a2478d6c2512
RidgeA <antongriadchenko@gmail.com>
2023-11-09 20:24:42 +0200

    rest api

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
  * https://expressjs.com/
  * https://koajs.com/
  * https://npmtrends.com/
+ * https://restfulapi.net/
+ * https://www.postman.com/
+ * https://insomnia.rest/
  */
 
 const Koa = require("koa");
@@ -10,12 +13,15 @@ const cors = require("@koa/cors");
 const { setTimeout } = require("node:timers/promises");
 const Router = require("@koa/router");
 const { bodyParser } = require("@koa/bodyparser");
+const { pbkdf2 } = require("node:crypto");
+const { promisify } = require("node:util");
+const pbkdf2Promise = promisify(pbkdf2);
 
 const app = new Koa();
 const router = new Router();
 
 app.use(cors());
-app.use(bodyParser({enableTypes:["json", "text"]}));
+app.use(bodyParser({ enableTypes: ["json", "text"] }));
 app.use(async (ctx, next) => {
   const start = performance.now();
 
@@ -26,48 +32,94 @@ app.use(async (ctx, next) => {
   console.log(`${ctx.method} ${ctx.path} - ${ctx.status} [${executionTime}ms]`);
 });
 
-// "http://localhost:3000/user?name=Name&password=password"
-// all|get|post|put|del|delete|patch
-router.post("/user", async (ctx, next) => {
+const usersDB = [];
 
-/*
-  const dataChunks = []
-  ctx.req.on("data", (chunk) => {
-    dataChunks.push(chunk);
-  });
+function withUserId(userId, ctx, next) {
+  const id = Number(userId);
 
-  ctx.req.on("end", () => {
-    const buffer = Buffer.concat(dataChunks);
-    const bodyString = buffer.toString()
-    console.log(bodyString);
-  })
-*/
-
-/*
-  const dataChunks = []
-  for await (const chunk of ctx.req) {
-    dataChunks.push(chunk);
+  if (id < 0 || Number.isNaN(id)) {
+    ctx.throw(400, "user id should be a number");
+    return;
   }
 
-  const buffer = Buffer.concat(dataChunks);
-  const bodyString = buffer.toString()
-  const {name, password} = JSON.parse(bodyString)
-*/
+  if (id >= usersDB.length) {
+    ctx.throw(404, `user with id "${id}" not found`);
+    return;
+  }
 
-  const { name, password } = ctx.request.body; // ctx.request.query
-  await setTimeout(100);
-  ctx.body = {
-    name,
-    password,
-  };
-});
+  ctx.state.userId = id;
+  return next();
+}
+
+// "http://localhost:3000/user?name=Name&password=password"
+// all|get|post|put|del|delete|patch
+router.prefix("/users")
+  .param("userId", withUserId)
+  // get all or subset of users
+  // GET http://localhost:3000/users
+  .get("/", (ctx, next) => {
+    ctx.body = usersDB;
+  })
+  // get user by id
+  // GET /users/1, /users/120c8e6a-9394-428a-9dac-8baa6e210410
+  .get("/:userId", (ctx, next) => {
+    ctx.body = usersDB[Number(ctx.state.userId)];
+  })
+  // create user
+  // POST /users
+  .post("/", async (ctx) => {
+    const { name, password } = ctx.request.body;
+
+    const hash = await pbkdf2Promise(password, "salt", 100_000, 64, "sha512");
+
+    const user = {
+      id: usersDB.length,
+      name,
+      password: hash.toString('hex'),
+    };
+
+    usersDB.push(user);
+
+    ctx.body = user;
+    ctx.status = 201;
+  })
+  // full update of user record
+  // PUT /users/1
+  .put("/:userId", (ctx, next) => {
+    const { name, password } = ctx.request.body;
+
+    const updatedUser = {
+      id: ctx.state.userId,
+      name,
+      password,
+    };
+
+    usersDB.splice(ctx.state.userId, 1, updatedUser);
+
+    ctx.body = updatedUser;
+  })
+  // partial update
+  // PATCH /users/1
+  .patch("/:userId", (ctx, next) => {
+  })
+  // delete user record
+  // DELETE /users/1
+  .delete("/:userId", (ctx, next) => {
+  });
+
+// router.post("/users",
+//   async (ctx, next) => {
+//     const { name, password } = ctx.request.body; // ctx.request.query
+//     await setTimeout(100);
+//     ctx.body = {
+//       name,
+//       password,
+//     };
+//   }
+// );
 
 app.use(router.middleware());
+app.use(router.allowedMethods());
 
 app.listen(3000);
 
-// POST /user HTTP 1.1
-// Content-Type: application/json
-// ...
-//
-// {"name":"John","password":"qwerty"}
